784c33964dccbe865c60e06709cfcf048a5f8978
copycatmay <maythril@gmail.com>
2024-02-09 14:19:48 -0500

    imagery update + timeline incorp

--- standard.js
+++ standard.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     // Existing click event
+
     $(".latLong").click(function(){
         var target = $(this).data("target");
         $("#" + target).toggleClass("visible");
         
+        $(this).toggleClass("clicked");
+        $(this).closest('.flex-container').find(".image img").toggle(); // Updated line
 
-      $(this).toggleClass("clicked");
-      $(this).next(".image").find("img").toggle();
-      updateReferencesTextColor(); // Call this function after toggling the class
+        updateReferencesTextColor(); // Call this function after toggling the class
     });
 
     // Smooth scrolling for inLinks
@@ -19,14 +20,6 @@ $(document).ready(function(){
         }, 1000); // Duration of the scroll animation in milliseconds
     });
 
-    // Function to increase spacing
-    function increaseSpacing() {
-      $('.latLong').each(function(i) {
-        // Increase the bottom margin for each .latLong element
-        // The multiplier (i + 1) ensures that the margin increases for each element
-        $(this).css('margin-bottom', (i + 1) * 150 + 'px');
-      });
-    }
 
     // Function to update references text color
     function updateReferencesTextColor() {
@@ -35,9 +28,4 @@ $(document).ready(function(){
       $('.relevantInfo').toggle(!hasBlackBox); // Hide or show .relavant-info
     }
 
-    // Initial call to set the spacing on page load
-    increaseSpacing();
-
-    // Optional: If you want to recalculate the spacing when the window is resized
-    $(window).resize(increaseSpacing);
   });
